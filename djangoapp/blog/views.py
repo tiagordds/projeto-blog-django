1b99e64a0fb436fee555903e1e2b499490619690
Tiago Rodrigues <yutri.trds@gmail.com>
2023-10-25 16:40:26 -0300

    1

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -21,7 +21,45 @@ def index(request):
     )
 
 
-def page(request):
+def created_by(request, author_pk):
+    posts = (
+        Post.objects.get_published()  # type: ignore
+        .filter(created_by__pk=author_pk)
+    )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def category(request, slug):
+    posts = (
+        Post.objects.get_published()  # type: ignore
+        .filter(category__slug=slug)
+    )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request, slug):
 
     return render(
         request,
@@ -32,11 +70,17 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
+    post = (
+        Post.objects.get_published()  # type: ignore
+        .filter(slug=slug)
+        .first()
+    )
+
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': post,
         }
     )
